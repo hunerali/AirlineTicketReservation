560cdffed25de7db2e6e1e4e7abc5e98fe8af312
hunerali <huner1442@gmail.com>
2022-09-18 19:19:06 +0400

    Step project

--- src/main/java/reservation/app/utility/RandomFlightGenerator.java
+++ src/main/java/reservation/app/utility/RandomFlightGenerator.java
@@ -2,7 +2,11 @@ package reservation.app.utility;
 
 import reservation.app.entity.Airlines;
 import reservation.app.entity.Cities;
+import reservation.app.entity.Flight;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomFlightGenerator {
@@ -19,6 +23,31 @@ public class RandomFlightGenerator {
 
     public static Cities destination() {
         return Cities.values()[rd.nextInt(Cities.values().length)];
+    }
+
+    public static LocalDateTime dateTime() {
+        return LocalDateTime.of(2022,
+                LocalDateTime.now().getMonth().getValue() + rd.nextInt(3) + 1,
+                rd.nextInt(28) + 1,
+                rd.nextInt(23) + 1, rd.nextInt(4) * 10);
+    }
+
+    public Flight generate() {
+        Airlines airline = airline();
+        Cities location = location();
+        Cities destination = destination();
+        LocalDateTime dateTime = dateTime();
+
+        return new Flight(id++, airline, location, destination, dateTime);
+    }
+
+    public List<Flight> generateRandomFlights(int size) {
+        List<Flight> flights = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Flight flight = generate();
+            flights.add(flight);
+        }
+        return flights;
     }
 
 
